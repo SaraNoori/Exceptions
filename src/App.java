a4eb3d49743ec2f757df017e695422f7783a6fbe
SaraNoori <sn1044604@student.francistuttle.edu>
2023-09-11 15:30:19 -0500

    Start of project

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public void Ask()
+    {
+        System.out.println("give a number:");
+        Scanner scan = new Scanner(System.in);
+        
+    }
+
 }
