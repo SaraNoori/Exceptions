a5eec63438571f1792db32a01c43e27b81f2f82e
SaraNoori <sn1044604@student.francistuttle.edu>
2023-09-13 14:47:33 -0500

    finished exceptions

--- src/App.java
+++ src/App.java
@@ -2,14 +2,52 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String answer = getResponse();
+        while (!answer.equals("exit"))
+        {
+            respond(answer);
+            answer = getResponse();
+        }
     }
 
-    public void Ask()
+    public static String getResponse()
     {
-        System.out.println("give a number:");
         Scanner scan = new Scanner(System.in);
-        
+        int parsed;
+
+        System.out.println("give a number:");
+        String answer = scan.nextLine();
+
+        return answer;
+    }
+
+    public static void respond(String answer)
+    {
+        int parsed;
+        try 
+        {
+            parsed = Integer.parseInt(answer);
+            System.out.println("The factors of " + answer + " are: ");
+            factor(parsed);
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println("not a number, please try again");
+        }
+    }
+
+    public static void factor(int number)
+    {
+
+        for (int i = 1; i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                System.out.print(i + " ");
+            }
+        }
+        // to make client side look neater
+        System.out.println();
     }
 
 }
